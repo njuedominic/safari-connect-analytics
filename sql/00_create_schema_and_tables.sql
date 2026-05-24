eeeb0de8f80b59383b85970b8e827b49f18e5212
njuedominic <njuemugodominic@gmail.com>
2026-05-24 19:05:02 +0300

    Create a schema and staging table

---
+++ sql/00_create_schema_and_tables.sql
@@ -0,0 +1,31 @@
+-- Create schema and tables for Safari Connect Database
+
+CREATE SCHEMA IF NOT EXISTS safari_connect;
+
+SET search_path TO safari_connect;
+
+DROP TABLE IF EXISTS bookings_staging;
+
+CREATE TABLE bookings_staging (
+    booking_id TEXT,
+    passenger_name TEXT,
+    passenger_phone TEXT,
+    passenger_gender TEXT,
+    passenger_city TEXT,
+    route_code TEXT,
+    route_from TEXT,
+    route_to TEXT,
+    vehicle_plate TEXT,
+    vehicle_type TEXT,
+    driver_name TEXT,
+    driver_rating TEXT,
+    departure_date TEXT,
+    departure_time TEXT,
+    seat_class TEXT,
+    seats_booked TEXT,
+    fare_per_seat TEXT,
+    total_fare TEXT,
+    payment_method TEXT,
+    booking_status TEXT,
+    trip_rating TEXT
+);
